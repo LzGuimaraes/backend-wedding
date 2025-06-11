2f226f5071878414f2bc19cb76744a4be97ceb53
LzGuimares <luizsantosleventi19901@gmail.com>
2025-06-11 15:42:43 -0400

    Remove dist/ from version control

--- dist/src/utils/helpers.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isValidEmail = void 0;
-const isValidEmail = (email) => {
-    // Implemente uma validação de e-mail mais robusta se necessário
-    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-};
-exports.isValidEmail = isValidEmail;
-//# sourceMappingURL=helpers.js.map
