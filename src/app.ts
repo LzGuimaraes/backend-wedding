4c21fe31231e0f7d7d54afb77ba2e8265a471191
Luiz <131723272+LzGuimaraes@users.noreply.github.com>
2025-06-12 18:27:05 +0000

    Ajustando para receber requisição apenas da rota http://localhost:3000

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response, NextFunction } from "express";
 import cors from "cors";
 import guestRoutes from "./routes/guestRoutes";
 import donationRoutes from "./routes/donationRoutes";
@@ -6,9 +6,18 @@ import giftRoutes from "./routes/giftRoutes";
 
 const app = express();
 
-// Middlewares
+// --- CONFIGURAÇÃO DO CORS ---
+
+const corsOptions = {
+  origin: "http://localhost:3000",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOptions));
+
+// Middlewares para parsear JSON
 app.use(express.json());
-app.use(cors());
 
 // Rotas da API
 app.use("/api/guests", guestRoutes);
@@ -16,10 +25,12 @@ app.use("/api/donations", donationRoutes);
 app.use("/api/gifts", giftRoutes);
 
 app.get("/", (req: Request, res: Response) => {
-  res.status(200).json({ message: "API do casamento ja esta quase pronta!" });
+  res.status(200).json({ message: "API do casamento já está quase pronta!" });
 });
 
-app.use((err: Error, req: Request, res: Response, next: any) => {
+// Middleware de Tratamento de Erros
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  // Importe NextFunction para tipagem
   console.error(err.stack);
   res.status(500).send("Something broke!");
 });
