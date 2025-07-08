f251e212b552d680766bfa1c29849b00b7d07d2f
Luiz <131723272+LzGuimaraes@users.noreply.github.com>
2025-07-07 20:46:07 -0400

    Ajustando conexão ao banco de dados

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,5 +1,4 @@
 // src/config/database.ts
-
 import { Pool } from "pg";
 
 const isProduction = process.env.NODE_ENV === "production";
@@ -12,19 +11,5 @@ const poolConfig = {
 };
 
 const pool = new Pool(poolConfig);
-
-// Teste de conexão com o banco de dados
-pool.connect((err, client, release) => {
-  if (err) {
-    return console.error("Error acquiring client from pool", err.stack);
-  }
-  client?.query("SELECT NOW()", (err, result) => {
-    release();
-    if (err) {
-      return console.error("Error executing test query", err.stack);
-    }
-    console.log("Database connection established:", result.rows[0].now);
-  });
-});
 
 export default pool;
